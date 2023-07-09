f7500fef93378f3788293eaabef78007017aa89e
5p00kk <szymon.kowalewski@gmail.com>
2023-07-09 17:56:26 +0200

    cleanup test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 
-from utils import es_rgb, es_veg
+from utils import es_rgb, es_veg, gen_request
 from copernicus import CopernicusAPI
 import numpy as np
 import cv2
@@ -10,49 +10,18 @@ def image_info(image):
 
 api = CopernicusAPI()
 
-request = {
-    "input": {
-        "bounds": {
-            "properties": {"crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84"},
-            "bbox": [
-                13.322174072265625,
-                45.85080395917834,
-                14.05963134765625,
-                46.29191774991382,
-            ],
-        },
-        "data": [
-            {
-                "type": "sentinel-2-l2a",
-                "dataFilter": {
-                    "timeRange": {
-                        "from": "2023-06-10T00:00:00Z",
-                        "to": "2023-07-10T00:00:00Z",
-                    }
-                },
-            }
-        ],
-    },
-    "output": {
-        "width": 2000,
-        "height": 2000,
-    },
-    "evalscript": es_rgb,
-}
+
+# Prepare request
+coords = [13.322174072265625, 45.85080395917834, 14.05963134765625, 46.29191774991382]
+time = ("2023-06-10T00:00:00Z", "2023-07-10T00:00:00Z")
+requests = [gen_request(coords, time, es_rgb), gen_request(coords, time, es_veg)]
 
 
-resp = api.request(request)
-np_array = np.frombuffer(resp.content, np.uint8)
-image = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
-image_info(image)
-
-
-request["evalscript"] = es_veg
-resp = api.request(request)
-np_array = np.frombuffer(resp.content, np.uint8)
-veg_image = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
-image_info(veg_image)
-
-cv2.imshow("ok", image)
-cv2.imshow("ok2", veg_image)
+for i, request in enumerate(requests):
+    resp = api.request(request)
+    np_array = np.frombuffer(resp.content, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
+    image_info(image)
+    cv2.imshow(f"req {i}", image)
+    cv2.waitKey(5)
 cv2.waitKey(-1)
