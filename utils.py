7da486c3d4672f6ab62be2bda169b77bf2377f7b
5p00kk <szymon.kowalewski@gmail.com>
2023-07-10 15:42:26 +0200

    add box calc class

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import math
+import numpy as np
 
 # Copernicus credentials
 COPERNICUS_ID = os.environ.get('COPERNICUS_ID')
@@ -75,6 +76,7 @@ def gen_request(coords, time, es):
                     "type": "sentinel-2-l2a",
                     "dataFilter": {
                         "timeRange": {"from": time[0], "to": time[1]},
+                        #"mosaickingOrder": "leastCC",
                     },
                 }
             ],
@@ -84,18 +86,42 @@ def gen_request(coords, time, es):
     }
     return request
 
-def latd_to_km(lat_d):
-    km = lat_d*(EARTH_CIRC/360)
-    return km
+def image_info(image):
+    print(f"Loaded image is {image.shape}, {image.dtype}, {image.shape[0]*image.shape[1]*image.shape[2]*image.itemsize}")
+    print(f"Num uniques {len(np.unique(image))}")
 
-def km_to_latd(km):
-    lat_d = km*(360/EARTH_CIRC)
-    return lat_d
+class BoxCalc:
+    def __init__(self, output_size, data_resolution) -> None:
+        self.resolution = data_resolution
+        self.output_size_px = output_size
+        self.output_size_km = output_size*data_resolution
+        self.coords = (0,0)
 
-def lond_to_km(lon_d, lat):
-    km = math.cos(math.radians(lat))*lon_d*(EARTH_CIRC/360)
-    return km
+    def get_box(self, center_pt):
+        lat_d = BoxCalc.km_to_latd(self.output_size_km)
+        lon_d = BoxCalc.km_to_lond(self.output_size_km, center_pt[1])
+        self.coords = [center_pt[0]-lon_d/2, center_pt[1]-lat_d/2, center_pt[0]+lon_d/2, center_pt[1]+lat_d/2]
+        return self.coords
+    
+    @staticmethod
+    def latd_to_km(lat_d):
+        km = lat_d*(EARTH_CIRC/360)
+        return km
+    
+    @staticmethod
+    def km_to_latd(km):
+        lat_d = km*(360/EARTH_CIRC)
+        return lat_d
+    
+    @staticmethod
+    def lond_to_km(lon_d, lat):
+        km = math.cos(math.radians(lat))*lon_d*(EARTH_CIRC/360)
+        return km
+    
+    @staticmethod
+    def km_to_lond(km, lat):
+        lon_d = (1/math.cos(math.radians(lat)))*(360/EARTH_CIRC)*km
+        return lon_d
 
-def km_to_lond(km, lat):
-    lon_d = (1/math.cos(math.radians(lat)))*(360/EARTH_CIRC)*km
-    return lon_d
+    def __repr__(self) -> str:
+        return f"BoxCalc [km]{self.resolution}, [km]{self.output_size_km}x{self.output_size_km} [px]{self.output_size_px}x{self.output_size_px}"
